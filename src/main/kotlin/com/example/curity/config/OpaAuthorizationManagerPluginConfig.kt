cc768d699293a679a7e12c4b1ba8698c11cc5f1f
iggbom <jonas@curity.io>
2023-01-11 13:04:14 -0800

    Added copyright statement

--- src/main/kotlin/com/example/curity/config/OpaAuthorizationManagerPluginConfig.kt
+++ src/main/kotlin/com/example/curity/config/OpaAuthorizationManagerPluginConfig.kt
@@ -1,5 +1,21 @@
 package com.example.curity.config;
 
+/*
+ *  Copyright 2023 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import se.curity.identityserver.sdk.config.Configuration
 import se.curity.identityserver.sdk.config.annotation.DefaultInteger
 import se.curity.identityserver.sdk.config.annotation.DefaultString
